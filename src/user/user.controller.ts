a75fe14fcf4666d9270fda6eb1fac1d9bc7a2ec9
Roman Emiliano Arenas <tuemail@ejemplo.com>
2024-11-21 19:54:22 -0300

    fix users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,113 +1,15 @@
-import {  Controller, Injectable } from '@nestjs/common'
-import * as bcrypt from 'bcrypt'
-import { User } from './entity/user.entity'
-import {
-  Repository,
-} from 'typeorm'
-import { InjectRepository } from '@nestjs/typeorm'
+import {  Controller, Get, HttpCode, Req } from '@nestjs/common'
 import { UserGuardDto } from 'src/common/dto/request/current.user.dto'
-import { UpdatePassword } from './dto/updatePassword.dto'
+import { UserService } from './user.service'
 
 
 @Controller('user')
 export class UserController {
-  currentUser: UserGuardDto
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-  ) {}
-
-  withUser(user: UserGuardDto) {
-    this.currentUser = user
-    return this
+  constructor(private userService: UserService) {}
+  @Get('test')
+  @HttpCode(200)
+  async test(@Req() req: Request & { user: UserGuardDto; lang: string }) {
+    return this.userService.test()
   }
 
-  async checkIfUserExists(params: {
-    email?: string
-    username?: string
-  }): Promise<User> {
-    const { email, username } = params
-    const conditions = [email && { email }, username && { username }]
-    return this.userRepository.findOne({
-      where: conditions,
-    })
-  }
-
-  async updatePassword(params: UpdatePassword) {
-    try {
-      const { password, newPassword, confirmPassword } = params
-      if (newPassword !== confirmPassword) {
-        throw new Error('Passwords do not match')
-      }
-      const user = await this.getUserById(this.currentUser.userId)
-      if (!user) {
-        throw new Error('User not found')
-      }
-      const isPasswordValid = await bcrypt.compare(password, user.password)
-      if (!isPasswordValid) {
-        throw new Error('Invalid password')
-      }
-      const hashedNewPassword = await bcrypt.hash(newPassword, 10)
-      user.password = hashedNewPassword
-      await this.userRepository.save(user)
-      return true
-    } catch (error) {
-     console.log(error)
-    }
-  }
-
-
-  async getUserMe() {
-    try {
-      const user = await this.userRepository.findOne({
-        where: {
-          usuario: this.currentUser.usuario,
-        }
-      })
-      if (!user) {
-        throw new Error('User not found')
-      }
-      return user
-    } catch (error) {
-        console.log(error)
-    }
-  }
-
-  async getUserById(id: string) {
-    try {
-      if (!id) {
-        throw new Error('Id is required')
-      }
-      const userFound = await this.userRepository.findOne({
-        where: {
-          id,
-        },
-      })
-      if (!userFound) {
-        throw new Error('User not found')
-      }
-      return userFound
-    } catch (error) {
-        console.log(error)
-    }
-  }
-
-
-  async getUserByEmail(email: string) {
-    try {
-      if (!email) {
-        throw new Error('Email is required')
-      }
-      const userFound = await this.userRepository.findOne({
-        where: {
-          email,
-        },
-      })
-      if (!userFound) {
-        throw new Error('User not found')
-      }
-      return userFound
-    } catch (error) {
-        console.log(error)
-    }
-  }}
+}
